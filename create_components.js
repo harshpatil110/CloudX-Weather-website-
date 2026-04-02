ec29a2ed01a413c12480b0957b6219399159fd19
harshpatil110 <2023.harshvardhan.patil@ves.ac.in>
2026-04-03 00:12:09 +0530

    feat: inject dynamic href links and convert mobile nav buttons to anchor tags in component generator

--- create_components.js
+++ create_components.js
@@ -30,6 +30,17 @@ function processHtmlFile(fileName, componentName) {
   bodyContent = bodyContent.replace(/style="([^"]*)"/g, "style={{}}"); // dirty hack but no inline styles were used so it's fine
 
   // If there's an active class logic for tabs, we could implement it, but for now we just dump the static component
+  // Link injection
+  bodyContent = bodyContent.replace(/href="#"(.*?)>([\s\S]*?)FORECAST/g, 'href="#forecast"$1>$2FORECAST');
+  bodyContent = bodyContent.replace(/href="#"(.*?)>([\s\S]*?)RADAR/g, 'href="#radar"$1>$2RADAR');
+  bodyContent = bodyContent.replace(/href="#"(.*?)>([\s\S]*?)AIR QUALITY/g, 'href="#aqi"$1>$2AIR QUALITY');
+  bodyContent = bodyContent.replace(/href="#"(.*?)>([\s\S]*?)HISTORICAL/g, 'href="#historical"$1>$2HISTORICAL');
+  // Also fix the bottom mobile nav
+  bodyContent = bodyContent.replace(/<button([^>]*)>([\s\S]*?)NOW<\/button>/g, '<a href="#forecast"$1>$2NOW</a>');
+  bodyContent = bodyContent.replace(/<button([^>]*)>([\s\S]*?)MAP<\/button>/g, '<a href="#radar"$1>$2MAP</a>');
+  bodyContent = bodyContent.replace(/<button([^>]*)>([\s\S]*?)AIR<\/button>/g, '<a href="#aqi"$1>$2AIR</a>');
+  bodyContent = bodyContent.replace(/<button([^>]*)>([\s\S]*?)ARCHIVE<\/button>/g, '<a href="#historical"$1>$2ARCHIVE</a>');
+
   let reactCode = `import React from 'react';\n\nexport default function ${componentName}() {\n  return (\n    <>\n${bodyContent}\n    </>\n  );\n}\n`;
 
   fs.writeFileSync(`src/components/${componentName}.js`, reactCode);
